a1cac8588dc6cb80ad7d2136e0b9b1362549adab
Tajman <tcohenhenry@umass.edu>
2019-10-14 23:43:05 -0400

    Site is now at Version 0.1. Added background and starting navbar.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.css'; 
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import NavDropdown from 'react-bootstrap/Nav';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Navbar bg="dark" variant="dark">
+    <Navbar.Brand href="#home">Menu</Navbar.Brand>  
+    <Nav className="mr-auto">
+      <Nav.Link href="#home">Home</Nav.Link>
+      <Nav.Link href="#Projects">Projects</Nav.Link>
+      <Nav.Link href="#About">About</Nav.Link>
+    </Nav> 
+  </Navbar>
+    </header>
     </div>
   );
 }
