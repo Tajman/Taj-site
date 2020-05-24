2381d640c84b56b42f06012457a163e0cc4b3363
Tajman <tcohenhenry@umass.edu>
2020-05-24 03:38:17 -0400

    a few changes

--- src/Components/pages/projects.js
+++ src/Components/pages/projects.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'; 
 import 'bootstrap/dist/css/bootstrap.min.css'; 
-import Container from 'react-bootstrap/Container'; 
+import Container from 'react-bootstrap/Container';
+import Card from 'react-bootstrap/Card'; 
+import CardDeck from 'react-bootstrap/Card';
 import "../../CSS/projects.css";
 
 export default class Projects extends Component {
@@ -11,8 +13,48 @@ export default class Projects extends Component {
             <h1> 
              Projects
             </h1> 
-          </Container> 
-       </header>
+                </Container>  
+
+                <Container className="band"> 
+                        <Card bg ='dark'>
+                            <Card.Img variant="top" src="holder.js/100px160" />
+                            <Card.Body>
+                                <Card.Title> Card title</Card.Title>
+                                <Card.Text>
+                                   "Something."
+                                </Card.Text>
+                            </Card.Body>
+                            <Card.Footer>
+                                
+                            </Card.Footer>
+                        </Card>
+                        <Card bg='dark'>
+                            <Card.Img variant="top" src="holder.js/100px160" />
+                            <Card.Body> 
+                                <Card.Title>Card title</Card.Title>
+                                <Card.Text>
+                                    Something.
+                                </Card.Text>
+                            </Card.Body>
+                            <Card.Footer>
+                                
+                            </Card.Footer>
+                        </Card>
+                        <Card bg ='dark'>
+                            <Card.Img variant="top" src="holder.js/100px160" />
+                            <Card.Body>
+                                <Card.Title>Card title</Card.Title>
+                                <Card.Text>
+                                    Something.    
+                                </Card.Text>
+                            </Card.Body>
+                            <Card.Footer>
+                                
+                            </Card.Footer>
+                        </Card> 
+
+                </Container>
+            </header> 
         );
     }
 }
--- src/Navbar.js
+++ src/Navbar.js
@@ -2,17 +2,17 @@ import React, { Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';  
-import { Link } from "react-router-dom"; 
+import { Link } from "react-router-dom";  
+import './CSS/Navbar.css';
 
 export default class NavBar extends Component {
     render() {
         return (
-            <Navbar bg="dark" variant="dark">
-    <Navbar.Brand href="#home">Menu</Navbar.Brand>  
+  <Navbar bg="dark" variant="dark">  
     <Nav>
-      <Link to='/'>Home</Link>
-      <Link to='/projects'>Projects</Link>
-      <Link to='/about'>About</Link>
+      <Link to='/' className="Navbar">Home</Link>
+      <Link to='/projects' className="Navbar">Projects</Link>
+      <Link to='/about' className="Navbar">About</Link>
     </Nav> 
   </Navbar>
         );
